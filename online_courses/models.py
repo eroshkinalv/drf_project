cc26891e4100ea5451719100507332cf830f83b7
Liudmila <leroshkina@mail.ru>
2025-01-07 10:48:41 +0300

    homework 30.1: user, online_courses apps; User, Course, Unit models

--- online_courses/models.py
+++ online_courses/models.py
@@ -12,9 +12,10 @@ class Course(models.Model):
 
 
 class Unit(models.Model):
-    name = models.CharField(max_length=200, unique=True, verbose_name='Название урока', help_text='Укажите название урока')
+    name = models.CharField(max_length=200, unique=True, verbose_name='Название урока')
     description = models.TextField(null=True, blank=True, verbose_name='Описание урока')
     image = models.ImageField(upload_to='unit_img/', null=True, blank=True, verbose_name='Превью (картинка)')
+    video = models.TextField(null=True, blank=True, verbose_name='Ссылка на видео')
 
     course = models.ForeignKey(Course, on_delete=models.SET, verbose_name='Название курса')
 
